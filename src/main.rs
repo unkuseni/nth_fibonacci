ab1a6304957b06bdb1444e7a66e269e4f51e4a40
unkuseni <amudalatadeoye@gmail.com>
2023-08-09 00:55:21 +0100

    trying out ideas

--- src/main.rs
+++ src/main.rs
@@ -24,3 +24,12 @@ fn main() {
         }
     }
 }
+fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
